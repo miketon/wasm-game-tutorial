8f84ca912cdc68a4ecc388024bfe1c9aad96282e
Mike Ton <mike.ton@gmail.com>
2024-09-24 08:52:36 -0500

    stubbing out black triangle to render on HTML 2D Context

--- www/index.js
+++ www/index.js
@@ -1,9 +1,24 @@
-import init, { greet } from '../pkg/walk_the_dog.js';
+// NOTE: should match the Cargo.toml package name, not directory name
+// 'rust_webpack_template' not 'walk-the-dog'
+import { main_js } from '../pkg/rust_webpack_template.js';
 
 async function run() {
-  await init();
-  const message = greet("World");
+  // Initialize the WebAssembly module
+  const wasm = main_js();
+
+  // Check what functions are available
+  console.log("Available exports : ", Object.keys(wasm));
+
+  // if main_js is available use it
+  if (wasm.main_js) {
+    console.log("main_js IS available");
+    main_js();
+  }
+  else {
+    console.log("main_js NOT available");
+  }
+  const message = ("Yo Wasm, World");
   document.getElementById('output').innerText = message;
 }
 
-run();
+run().catch(console.error);
