096cbcafb9e0383fe502e76c689059a1aec3bf50
Mike Ton <mike.ton@gmail.com>
2024-09-26 10:23:04 -0500

    lib.rs : break triangle into discrete functions, move inline values -> const

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::CanvasRenderingContext2d;
+use web_sys::{window, CanvasRenderingContext2d, HtmlCanvasElement};
+
+struct HtmlConst;
+
+impl HtmlConst {
+    const CANVAS_ID: &'static str = "canvas";
+    const CONTEXT_2D: &'static str = "2d";
+}
+
+const TRIANGLE_LENGTH: f64 = 600.0;
 
 #[wasm_bindgen]
 pub fn main_js() -> Result<(), JsValue> {
@@ -8,43 +17,56 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
     // get context
-    let window = web_sys::window().unwrap();
+    let window = window().expect("Failed to get window");
     // TODO-done: unwrap -> expect for more explicit error messages
     let document = window.document().expect("document on window required");
     let canvas = document
-        .get_element_by_id("canvas")
+        .get_element_by_id(HtmlConst::CANVAS_ID)
         .expect("canvas element required")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .dyn_into::<HtmlCanvasElement>()
         .expect("HtmlCanvasElement required");
     let context = canvas
-        .get_context("2d")
+        .get_context(HtmlConst::CONTEXT_2D)
         .expect("2d context required")
         .expect("context should exist")
-        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .dyn_into::<CanvasRenderingContext2d>()
         .expect("context should be a CanvasRenderingContext2d");
 
     // draw @context
     // - add '?' end of function if function returns a Result<>
     // - web assembly code interacting with javascript can fail silently,
     // returning Result<> fails loudly as a forcing factor to fix
-    draw_triangle(&context)?;
+    let triangle_points = compute_triangle_points(TRIANGLE_LENGTH);
+    draw_triangle(&context, triangle_points)?;
 
     Ok(())
 }
 
-fn draw_triangle(context: &CanvasRenderingContext2d) -> Result<(), JsValue> {
-    // get to start position
-    context.move_to(600.0, 0.0); // top of triangle
+fn draw_triangle(
+    context: &CanvasRenderingContext2d,
+    points: [(f64, f64); 3],
+) -> Result<(), JsValue> {
+    // destructuring for clarity
+    let [top, left, right] = points;
 
-    // draw triangle
+    // path out triangle
     context.begin_path();
-    context.line_to(0.0, 600.0); // bottom left of triangle
-    context.line_to(600.0, 600.0); // bottom right of triangle
-    context.line_to(300.0, 0.0); // back to top of triangle
-                                 // close and fill shape
+    context.move_to(top.0, top.1);
+    context.line_to(left.0, left.1);
+    context.line_to(right.0, right.1);
     context.close_path();
+
     context.stroke();
-    context.fill();
+    // context.fill();
 
     Ok(())
+}
+
+/// return 3 points of equilateral triangle given length
+fn compute_triangle_points(length: f64) -> [(f64, f64); 3] {
+    [
+        (length / 2.0, 0.0), // top
+        (0.0, length),       // bottom-left
+        (length, length),    // bottom-right
+    ]
 }
