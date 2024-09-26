ee78e80c1d76fcdeef4f39dc19faae87631add02
Mike Ton <mike.ton@gmail.com>
2024-09-26 07:11:46 -0500

    lib.rs : unwrap -> expect, index.html -> fixed extra canvas tag, prettier lint

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,9 @@
 use wasm_bindgen::prelude::*;
-use web_sys::{
-    CanvasRenderingContext2d,
-};
 use wasm_bindgen::JsCast;
+use web_sys::CanvasRenderingContext2d;
 
 #[wasm_bindgen]
-pub fn main_js()-> Result<(), JsValue> {
-
+pub fn main_js() -> Result<(), JsValue> {
     // setup better panic messages for debugging
     console_error_panic_hook::set_once();
 
@@ -19,7 +16,8 @@ pub fn main_js()-> Result<(), JsValue> {
         .expect("canvas element required")
         .dyn_into::<web_sys::HtmlCanvasElement>()
         .expect("HtmlCanvasElement required");
-    let context = canvas.get_context("2d")
+    let context = canvas
+        .get_context("2d")
         .expect("2d context required")
         .expect("context should exist")
         .dyn_into::<web_sys::CanvasRenderingContext2d>()
@@ -28,7 +26,7 @@ pub fn main_js()-> Result<(), JsValue> {
     // draw @context
     // - add '?' end of function if function returns a Result<>
     // - web assembly code interacting with javascript can fail silently,
-    // returning Result<> fails loudly as a forcing factor to fix 
+    // returning Result<> fails loudly as a forcing factor to fix
     draw_triangle(&context)?;
 
     Ok(())
@@ -36,17 +34,17 @@ pub fn main_js()-> Result<(), JsValue> {
 
 fn draw_triangle(context: &CanvasRenderingContext2d) -> Result<(), JsValue> {
     // get to start position
-    context.move_to(600.0, 0.0);    // top of triangle
+    context.move_to(600.0, 0.0); // top of triangle
 
     // draw triangle
     context.begin_path();
-    context.line_to(0.0, 600.0);    // bottom left of triangle
-    context.line_to(600.0, 600.0);  // bottom right of triangle
-    context.line_to(300.0, 0.0);    // back to top of triangle
-    // close and fill shape
+    context.line_to(0.0, 600.0); // bottom left of triangle
+    context.line_to(600.0, 600.0); // bottom right of triangle
+    context.line_to(300.0, 0.0); // back to top of triangle
+                                 // close and fill shape
     context.close_path();
     context.stroke();
     context.fill();
-    
+
     Ok(())
 }
--- www/index.js
+++ www/index.js
@@ -1,14 +1,13 @@
 // NOTE: should match the Cargo.toml package name, not directory name
 // 'rust_webpack_template' not 'walk-the-dog'
-import { main_js } from '../pkg/rust_webpack_template.js';
+import { main_js } from "../pkg/rust_webpack_template.js";
 
 async function run() {
   // Initialize the WebAssembly module
   main_js();
 
-  const message = ("Yoyo Wasm, World");
-  document.getElementById('output').innerText = message;
-
+  const message = "Yoyo Wasm, World";
+  document.getElementById("output").innerText = message;
 }
 
 run().catch(console.error);
