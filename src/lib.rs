da8fcdc25c81ac807db48224c9de6c5e927bdbd0
Mike Ton <mike.ton@gmail.com>
2024-10-20 08:22:24 -0500

    [lib.rs] removed png draw so only sprite sheet draw remains

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 // ==================== Imports ====================
-use std::rc::Rc;
+use serde::Deserialize;
+use serde_wasm_bindgen::from_value;
 use std::cell::RefCell;
 use std::collections::HashMap;
-use serde::Deserialize;
+use std::rc::Rc;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use serde_wasm_bindgen::from_value;
 
-// FIXME: include and activate sierpinksi triangle
-
+// OOOF: include and activate sierpinksi triangle
 // ELI5: Javascript properties are public and web_sys :
 // - a) just generates setter and getter functions
 // - b) these functions take JsValue objects that represent objects owned by
@@ -33,7 +32,6 @@ mod html {
 }
 
 // ==================== Structs ====================
-
 #[derive(Deserialize)]
 struct Cell {
     frame: Rect,
@@ -57,22 +55,17 @@ impl Rect {
     // Rect
     // ├─ x
     // ├─ y
-    // ├─ width
-    // ├─ height
+    // ├─ w
+    // ├─ h
     // ├─ new()
     // └─ center()
     // NOTE: The document Symbols table will VISUALLY list them as SIBLINGS
     // for navigating convenience
     fn new(x: f64, y: f64, w: f64, h: f64) -> Self {
-        Self {
-            x,
-            y,
-            w,
-            h,
-        }
+        Self { x, y, w, h }
     }
     fn center(&self) -> (f64, f64) {
-        (self.x + self.w * 0.5, self.y + self.h* 0.5)
+        (self.x + self.w * 0.5, self.y + self.h * 0.5)
     }
 }
 
@@ -88,7 +81,6 @@ pub fn main_js() -> Result<(), JsValue> {
 
     // get context
     let window = window().expect("Failed to get window");
-    // TODO-done: unwrap -> expect for more explicit error messages
     let document = window.document().expect("document on window required");
     let canvas = document
         .get_element_by_id(html::CANVAS_ID)
@@ -97,35 +89,40 @@ pub fn main_js() -> Result<(), JsValue> {
         .expect("HtmlCanvasElement required");
     let context = canvas
         .get_context(html::CONTEXT_2D)
+        // NOTE: Explian why we are unwrapping twice here?
         .expect("2d context required")
         .expect("context should exist")
         .dyn_into::<CanvasRenderingContext2d>()
         .expect("context should be a CanvasRenderingContext2d");
 
-
-    // spawns a new asynchronous task in local thread, for web assembly 
+    // spawns a new asynchronous task in local thread, for web assembly
     // environment, using wasm_bindgen_futures
     wasm_bindgen_futures::spawn_local(
         // starts an asynchronous closure
-        // - 'move' keyword indicates that the closure will take ownership of 
+        // - 'move' keyword indicates that the closure will take ownership of
         // any variables it uses from the surrounding scope
         async move {
             let image = HtmlImageElement::new().expect("HtmlImageElement required");
+            let json = fetch_json("rhb.json")
+                .await
+                .expect("Could not fetch rhb.json");
+            let sheet: Sheet =
+                from_value(json).expect("Could not convert rhb.json into a Sheet structure");
 
-            // creates a one-shot channel : 
+            // creates a one-shot channel :
             // - it's a single-use channel between asynchronous tasks
             // ELI5: help me understand this
-            // ANSWER : This helps coordinate actions between different parts 
-            // of your program, especially when one part needs to wait for 
+            // ANSWER : This helps coordinate actions between different parts
+            // of your program, especially when one part needs to wait for
             // another part to finish something.
             // Here's how it works:
             // - The channel::<()>() function creates this special walkie-talkie set.
             // - It gives you two parts:
-            //  - tx (transmitter): This is your part of the walkie-talkie. 
+            //  - tx (transmitter): This is your part of the walkie-talkie.
             //  You'll use this to send the message.
-            //  - rx (receiver): This is your friend's part. They'll use this 
+            //  - rx (receiver): This is your friend's part. They'll use this
             //  to listen for your message.
-            // - You can only use this walkie-talkie set once. After you send 
+            // - You can only use this walkie-talkie set once. After you send
             // a message, it stops working.
             let (tx, rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
             // NOTE: RefCell vs Mutex because we are ASSUMING single thread
@@ -135,15 +132,13 @@ pub fn main_js() -> Result<(), JsValue> {
             let error_tx = success_tx.clone();
 
             // Callback closures that will be called once the load attempt done
-            // - Closure::once() EXPLICITLY specifies a FnOnce closure, but tx 
-            // send() call would have IMPLICITLY specified a FnOnce closure
-            // at compile time
+            // - Closure::once() EXPLICITLY specifies a FnOnce closure, else ...
+            // - NOTE: send() call IMPLICITLY have compiled to a FnOnce closure
             let callback_success = Closure::once(move || {
-                if let Some(tx) = success_tx.borrow_mut().take(){
+                if let Some(tx) = success_tx.borrow_mut().take() {
                     let _ = tx.send(Ok(()));
                 }
             });
-
             let callback_error = Closure::once(move |err: Event| {
                 if let Some(tx) = error_tx.borrow_mut().take() {
                     let _ = tx.send(Err(err.into()));
@@ -151,71 +146,19 @@ pub fn main_js() -> Result<(), JsValue> {
             });
 
             // Sets the callbacks
-            image.set_onload(Some(callback_success.as_ref()
-                // unchecked_ref is used to convert the call back to the 
-                // correct type expected by 'set_onload'
-                .unchecked_ref()));
-            image.set_onerror(Some(callback_error.as_ref().unchecked_ref()));
-
-            // start loading the image by setting the source to our file name
-            image.set_src("Idle (1).png");
-            // wait for image to load or fail - (invisible progress bar)
-            // - pauses execution until signal is received in one-shot channel
-            // - walkie talkie are cleared because it was one-shot
-            // - forget() call no longer necessary
-            match rx.await {
-                Ok(Ok(()))=> {
-                    // Image loaded successfully
-                    console::log_1(&JsValue::from_str("[image] loading : DONE"));
-                    // Drawing operations here
-                    context.draw_image_with_html_image_element(&image, 0.0, 0.0)
-                        .expect("Failed to draw image");
-                },
-                Ok(Err(err))=>{
-                    // Image failed to load
-                    console::log_1(&JsValue::from_str(&format!("[image] loading : ERROR {:?}", err)));
-                },
-                Err(_)=>{
-                    // Channel closed without sending a value
-                    console::log_1(&JsValue::from_str("[image] loading : NO VALUE SENT"));
-                }
-            };
-
-            let _ = context.draw_image_with_html_image_element(&image, 0.0, 0.0);
-            let json = fetch_json("rhb.json")
-                .await
-                .expect("Could not fetch rhb.json");
-
-            let sheet: Sheet = from_value(json)
-                .expect("Could not convert rhb.json into a Sheet structure");
-
-            let (tx, rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
-            let success_tx = Rc::new(RefCell::new(Some(tx)));
-            let error_tx = success_tx.clone();
-
-            let callback_success = Closure::once(move || {
-                if let Some(tx) = success_tx.borrow_mut().take(){
-                    let _ = tx.send(Ok(()));
-                }
-            });
-
-            let callback_error = Closure::once(move |err: Event| {
-                if let Some(tx) = error_tx.borrow_mut().take() {
-                    let _ = tx.send(Err(err.into()));
-                }
-            });
-
-            // Sets the callbacks
-            image.set_onload(Some(callback_success.as_ref()
-                // unchecked_ref is used to convert the call back to the 
-                // correct type expected by 'set_onload'
-                .unchecked_ref()));
+            image.set_onload(Some(
+                callback_success
+                    .as_ref()
+                    // unchecked_ref is used to convert the call back to the
+                    // correct type expected by 'set_onload'
+                    .unchecked_ref(),
+            ));
             image.set_onerror(Some(callback_error.as_ref().unchecked_ref()));
 
             // start loading the image by setting the source to our file name
             image.set_src("rhb.png");
             match rx.await {
-                Ok(Ok(()))=> {
+                Ok(Ok(())) => {
                     console::log_1(&JsValue::from_str("[json] loading : DONE"));
                     let sprite = sheet.frames.get("Slide (1).png").expect("Cell not found");
                     // Drawing operations here
@@ -229,28 +172,26 @@ pub fn main_js() -> Result<(), JsValue> {
                         300.0,
                         sprite.frame.w,
                         sprite.frame.h,
-
                     ).expect("Failed to draw image");
-                },
-                Ok(Err(err))=>{
-                    console::log_1(&JsValue::from_str(&format!("[json] loading : ERROR {:?}", err)));
-                },
-                Err(_)=>{
+                }
+                Ok(Err(err)) => {
+                    console::log_1(&JsValue::from_str(&format!(
+                        "[json] loading : ERROR {:?}",
+                        err
+                    )));
+                }
+                Err(_) => {
                     console::log_1(&JsValue::from_str("[json] loading : NO VALUE SENT"));
                 }
             };
-
-        }
+        },
     );
-
     Ok(())
 }
 
-async fn fetch_json(json_path:&str) -> Result<JsValue, JsValue>{
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
     let window = window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(
-        window.fetch_with_str(json_path)
-    ).await?;
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
 
     let resp: web_sys::Response = resp_value.dyn_into()?;
     wasm_bindgen_futures::JsFuture::from(resp.json()?).await
